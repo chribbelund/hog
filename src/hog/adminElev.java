308aa98d40c0007fba56df20e892a29069c3916a
Christoffer Lundqvist <chribbelundqvist@gmail.com>
2018-12-27 12:23:59 +0100

    jekla massa på adminElev

--- src/hog/adminElev.java
+++ src/hog/adminElev.java
@@ -5,17 +5,29 @@
  */
 package hog;
 
+import javax.swing.JOptionPane;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author Jamie
  */
 public class adminElev extends javax.swing.JFrame {
 
+    private InfDB idb;
+
     /**
      * Creates new form adminElev
      */
     public adminElev() {
         initComponents();
+        try {
+            idb = new InfDB("C:\\db\\HOGDB.FDB");
+        } catch (InfException undantag) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + undantag.getMessage());
+        }
     }
 
     /**
@@ -28,6 +40,19 @@ public class adminElev extends javax.swing.JFrame {
     private void initComponents() {
 
         btnTillbaka = new javax.swing.JButton();
+        btnAddElev = new javax.swing.JButton();
+        btnChangeElev = new javax.swing.JButton();
+        btnDeleteElev = new javax.swing.JButton();
+        txtFaltFornamn = new javax.swing.JTextField();
+        txtFaltEfternamn = new javax.swing.JTextField();
+        txtFaltSovsal = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        btnAndraPrefekt = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtSvar = new javax.swing.JTextArea();
+        cboxElevhem = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -38,19 +63,124 @@ public class adminElev extends javax.swing.JFrame {
             }
         });
 
+        btnAddElev.setText("Registrera Elev");
+        btnAddElev.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddElevActionPerformed(evt);
+            }
+        });
+
+        btnChangeElev.setText("Ändra Elevinfo");
+        btnChangeElev.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnChangeElevActionPerformed(evt);
+            }
+        });
+
+        btnDeleteElev.setText("Ta bort Elev");
+        btnDeleteElev.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteElevActionPerformed(evt);
+            }
+        });
+
+        txtFaltFornamn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtFaltFornamnActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Förnamn");
+
+        jLabel2.setText("Efternamn");
+
+        jLabel3.setText("Sovsal");
+
+        btnAndraPrefekt.setText("Ändra Prefekt");
+        btnAndraPrefekt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAndraPrefektActionPerformed(evt);
+            }
+        });
+
+        txtSvar.setEditable(false);
+        txtSvar.setColumns(20);
+        txtSvar.setRows(5);
+        jScrollPane1.setViewportView(txtSvar);
+
+        cboxElevhem.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Slytherin", "Hufflepuff", "Gryffindor", "Ravenclaw" }));
+        cboxElevhem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cboxElevhemActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24))
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btnTillbaka)
-                .addContainerGap(302, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(btnChangeElev, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(btnAndraPrefekt, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnTillbaka)
+                        .addComponent(btnAddElev, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnDeleteElev, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtFaltFornamn, javax.swing.GroupLayout.DEFAULT_SIZE, 77, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtFaltEfternamn, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(txtFaltSovsal, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(64, 64, 64))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(cboxElevhem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(262, Short.MAX_VALUE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(btnAddElev)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnDeleteElev))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel3))
+                        .addGap(7, 7, 7)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtFaltFornamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtFaltEfternamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtFaltSovsal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnAndraPrefekt)
+                    .addComponent(cboxElevhem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnChangeElev)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12)
                 .addComponent(btnTillbaka)
                 .addContainerGap())
         );
@@ -63,12 +193,96 @@ public class adminElev extends javax.swing.JFrame {
         dispose();
     }//GEN-LAST:event_btnTillbakaActionPerformed
 
+    private void btnChangeElevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnChangeElevActionPerformed
+        new adminElevInfo().setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnChangeElevActionPerformed
+
+    private void btnAddElevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddElevActionPerformed
+        try {
+            String fornamn = txtFaltFornamn.getText();
+            String efternamn = txtFaltEfternamn.getText();
+            String sovsal = txtFaltSovsal.getText();
+
+            //Combobox för att välja sovsal?
+            if (!fornamn.isEmpty() && !efternamn.isEmpty() && !sovsal.isEmpty()) {
+                String increment = idb.getAutoIncrement("ELEV", "ELEV_ID");
+                System.out.println(increment);
+                String fraga = "INSERT INTO ELEV VALUES (" + "" + increment + " " + ", '" + fornamn + "', '" + efternamn + "', '" + sovsal + "');";
+                System.out.println(fraga);
+                idb.insert(fraga);
+
+                txtSvar.setText("En ny elev har registrerats");
+            }
+        } catch (InfException undantag) { //om databasen inte hittas så kommer ett felmeddelande upp
+            JOptionPane.showMessageDialog(null, "Något gick fel!");
+            System.out.println("Internt felmeddelande" + undantag.getMessage());
+        }
+    }//GEN-LAST:event_btnAddElevActionPerformed
+
+    private void txtFaltFornamnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtFaltFornamnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtFaltFornamnActionPerformed
+
+    private void btnDeleteElevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteElevActionPerformed
+        try {
+            String fornamn = txtFaltFornamn.getText();
+            String efternamn = txtFaltEfternamn.getText();
+
+            String fraga = "Delete from ELEV where FORNAMN = '" + fornamn + "' AND EFTERNAMN = '" + efternamn + "'; "; //Tar bort raden med givet för- och efternamn.
+            System.out.println(fraga);
+            idb.delete(fraga); //Uppdaterar databasen.
+            txtSvar.setText("En elev har tagits bort");
+
+        } catch (InfException undantag) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + undantag.getMessage());
+        }
+    }//GEN-LAST:event_btnDeleteElevActionPerformed
+
+    private void cboxElevhemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboxElevhemActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cboxElevhemActionPerformed
+
+    private void btnAndraPrefektActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAndraPrefektActionPerformed
+
+        //Validering som kollar att eleven är med i det elevhem som hen ska bli prefekt i
+        try {
+            String itemText = (String) cboxElevhem.getSelectedItem(); //Tar det valda värdet ur comboxboxen och sätter det i en sträng
+            String fornamn = txtFaltFornamn.getText();
+            String efternamn = txtFaltEfternamn.getText();
+
+            String fraga = "SELECT ELEV_ID FROM ELEV WHERE FORNAMN = '" + fornamn + "' AND EFTERNAMN = '" + efternamn + "'; ";
+            String elevid = idb.fetchSingle(fraga);
+
+            fraga = "UPDATE ELEVHEM SET FORESTANDARE = '" + elevid + "' WHERE ELEVHEMSNAMN = '" + itemText + "';";
+            System.out.println(fraga);
+            idb.update(fraga);
+            txtSvar.setText("En ny prefekt har registrerats");
+        } catch (InfException undantag) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + undantag.getMessage());
+        }
+    }//GEN-LAST:event_btnAndraPrefektActionPerformed
+
     /**
      * @param args the command line arguments
      */
-  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAddElev;
+    private javax.swing.JButton btnAndraPrefekt;
+    private javax.swing.JButton btnChangeElev;
+    private javax.swing.JButton btnDeleteElev;
     private javax.swing.JButton btnTillbaka;
+    private javax.swing.JComboBox<String> cboxElevhem;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField txtFaltEfternamn;
+    private javax.swing.JTextField txtFaltFornamn;
+    private javax.swing.JTextField txtFaltSovsal;
+    private javax.swing.JTextArea txtSvar;
     // End of variables declaration//GEN-END:variables
 }
