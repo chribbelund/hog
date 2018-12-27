74c33d7dbf96f28d96a50cfd901bf539ff4b2952
DrejarN <30621704+DrejarN@users.noreply.github.com>
2018-12-27 16:48:49 +0100

    Tog bort elevBetygAndra, satt in funktionaliteten i elevBetygReg. Metoderna behöver validering

--- src/hog/elevBetygReg.java
+++ src/hog/elevBetygReg.java
@@ -51,8 +51,9 @@ public class elevBetygReg extends javax.swing.JFrame {
         cboxBetyg = new javax.swing.JComboBox<>();
         jScrollPane1 = new javax.swing.JScrollPane();
         txtSvar = new javax.swing.JTextArea();
-        btnUppdatera = new javax.swing.JButton();
+        btnNyttBetyg = new javax.swing.JButton();
         txtKursnamn = new javax.swing.JTextField();
+        btnUppdateraBetyg = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -63,7 +64,7 @@ public class elevBetygReg extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Registrera kursbetyg för elev");
+        jLabel1.setText("Hantera kursbetyg för elev");
 
         jLabel2.setText("Förnamn");
 
@@ -85,10 +86,17 @@ public class elevBetygReg extends javax.swing.JFrame {
         txtSvar.setRows(5);
         jScrollPane1.setViewportView(txtSvar);
 
-        btnUppdatera.setText("OK");
-        btnUppdatera.addActionListener(new java.awt.event.ActionListener() {
+        btnNyttBetyg.setText("Skapa nytt");
+        btnNyttBetyg.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnUppdateraActionPerformed(evt);
+                btnNyttBetygActionPerformed(evt);
+            }
+        });
+
+        btnUppdateraBetyg.setText("Uppdatera befintligt");
+        btnUppdateraBetyg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUppdateraBetygActionPerformed(evt);
             }
         });
 
@@ -106,7 +114,7 @@ public class elevBetygReg extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addComponent(jLabel3))
                             .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 110, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 255, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -115,7 +123,10 @@ public class elevBetygReg extends javax.swing.JFrame {
                                 .addComponent(txtFornamn, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
                                 .addComponent(txtEfternamn, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(btnUppdatera)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnNyttBetyg)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnUppdateraBetyg))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addGroup(layout.createSequentialGroup()
@@ -157,7 +168,9 @@ public class elevBetygReg extends javax.swing.JFrame {
                     .addComponent(cboxBetyg, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtKursnamn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(26, 26, 26)
-                .addComponent(btnUppdatera)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnNyttBetyg)
+                    .addComponent(btnUppdateraBetyg))
                 .addGap(88, 88, 88)
                 .addComponent(btnTillbaka)
                 .addContainerGap())
@@ -171,8 +184,49 @@ public class elevBetygReg extends javax.swing.JFrame {
         //dispose();
     }//GEN-LAST:event_btnTillbakaActionPerformed
 
-    private void btnUppdateraActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUppdateraActionPerformed
-        //Ska uppdatera en unges betyg. OTESTAD!
+    private void btnNyttBetygActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNyttBetygActionPerformed
+        //Registrerar ett nytt betyg för en elev på en kurs. OTESTAD!
+        //Måste ha en kontrollfunktion som kollar att ett betyg inte redan finns för eleven på den kursen.
+        
+        
+        try {
+            String fornamn = txtFornamn.getText();
+            String efternamn = txtEfternamn.getText();
+            String kursnamn = txtKursnamn.getText();
+            String betyget = (String) cboxBetyg.getSelectedItem(); //Hämtar valet och sätter det i en sträng.
+            String betygKort = betyget.substring(0,1);
+            System.out.println(betygKort);
+           
+            
+            //Hitta elevens elev id basserat på namnet
+            String fraga = "SELECT ELEV_ID FROM ELEV WHERE FORNAMN = '" + fornamn + "' AND EFTERNAMN = '" + efternamn + "'; ";
+            String elevID = idb.fetchSingle(fraga);
+            
+            //Hitta kursens kursID baserat på kursnamn
+            String fraga2 = "SELECT KURS_ID FROM KURS WHERE KURSNAMN = '" + kursnamn + "'; ";
+            String kursID = idb.fetchSingle(fraga2);
+            
+            //Uppdatera kursbetyget
+            String fraga3 = "UPDATE HAR_BETYG_I SET KURSBETYG = '" + betygKort + "' WHERE ELEV_ID = '" + elevID + "' AND KURS_ID = '" + kursID + "' ";
+            idb.update(fraga3);
+            
+            txtSvar.setText("Betyget " + betyget + " har registrerats på " + kursnamn + " för " + fornamn);
+            
+        
+        } catch(InfException e) {
+            JOptionPane.showMessageDialog(null, "Något gick fel");
+            System.out.println("Internt felmeddelande" + e.getMessage());
+        }
+        
+    }//GEN-LAST:event_btnNyttBetygActionPerformed
+
+    private void txtFornamnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtFornamnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtFornamnActionPerformed
+
+    private void btnUppdateraBetygActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUppdateraBetygActionPerformed
+        //Ska ändra ett gammalt betyg om det finns. OTESTAD!
+        //Den här metoden behöver en kontrollfunktion för att se om tidigare betyg finns.
         
         try {
             String fornamn = txtFornamn.getText();
@@ -201,12 +255,7 @@ public class elevBetygReg extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Något gick fel");
             System.out.println("Internt felmeddelande" + e.getMessage());
         }
-        
-    }//GEN-LAST:event_btnUppdateraActionPerformed
-
-    private void txtFornamnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtFornamnActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtFornamnActionPerformed
+    }//GEN-LAST:event_btnUppdateraBetygActionPerformed
 
     /**
      * @param args the command line arguments
@@ -214,8 +263,9 @@ public class elevBetygReg extends javax.swing.JFrame {
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnNyttBetyg;
     private javax.swing.JButton btnTillbaka;
-    private javax.swing.JButton btnUppdatera;
+    private javax.swing.JButton btnUppdateraBetyg;
     private javax.swing.JComboBox<String> cboxBetyg;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
