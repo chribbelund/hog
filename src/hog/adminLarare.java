e72648dc34ddb6f23206a08dec767ee1d3c00acd
konny2012 <39331850+konny2012@users.noreply.github.com>
2019-01-12 15:55:01 +0100

    fixat ta bort lärare

--- src/hog/adminLarare.java
+++ src/hog/adminLarare.java
@@ -208,66 +208,59 @@ public class adminLarare extends javax.swing.JFrame {
     //Måste kolla hur vi ska göra? Antar exempelvis att historik om att en lärare hållt en kurs ska sparas
     private void btnTaBortLarareActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTaBortLarareActionPerformed
 
-            String larare = (String) cboxLarare.getSelectedItem(); //Tar lärarnamnet från comboboxen
-            String larareFornamn = larare.split(" ")[0]; //Delar upp namnet i två strings
-            String larareEfternamn = larare.split(" ")[1];
-            
-            String lararid = "";
-            String arforestandare = null;
-            String kompetens = null;
-            String kurslarare = null;
- 
+        String larare = (String) cboxLarare.getSelectedItem(); //Tar lärarnamnet från comboboxen
+        String larareFornamn = larare.split(" ")[0]; //Delar upp namnet i två strings
+        String larareEfternamn = larare.split(" ")[1];
+
+        String lararid = null;
+        String arforestandare = null;
+        String kompetens = null;
+        String kurslarare = null;
+
+        try {
+            lararid = idb.fetchSingle("SELECT LARAR_ID FROM LARARE WHERE FORNAMN = '" + larareFornamn + "' AND EFTERNAMN = '" + larareEfternamn + "' ");
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(rootPane, "Kunde ej hitta angivna lärarens id");
+        }
+        try {
+            arforestandare = idb.fetchSingle("SELECT ELEVHEMSNAMN FROM ELEVHEM WHERE FORESTANDARE = " + lararid);
+        } catch (InfException e) {
+        }
+        try {
+            kompetens = idb.fetchSingle("SELECT LARAR_ID FROM HAR_KOMPETENS_I WHERE LARAR_ID = " + lararid);
+
+        } catch (InfException e) {
+        }
+        try {
+            kurslarare = idb.fetchSingle("SELECT KURSLARARE FROM KURS WHERE KURSLARARE = " + lararid);
+        } catch (InfException e) {
+        }
+
+        if (validering.kollaStringVarde(kompetens)) {
             try {
-                lararid = idb.fetchSingle("SELECT LARAR_ID FROM LARARE WHERE FORNAMN = '" + larareFornamn + "' AND EFTERNAMN = '" + larareEfternamn + "' ");
-            } 
-            catch(InfException e){
-                JOptionPane.showMessageDialog(rootPane, "Kunde ej hitta angivna lärarens id");
-            }
-            try {
-                arforestandare = idb.fetchSingle("SELECT ELEVHEMSNAMN FROM ELEVHEM WHERE FORESTANDARE = "+lararid);
-            }
-            catch(InfException e){
-            }
-            try{
-            kompetens = idb.fetchSingle("SELECT LARAR_ID FROM HAR_KOMPETENS_I WHERE LARAR_ID = "+lararid);
-            }
-            catch(InfException e){
-            }
-            try {
-                kurslarare = idb.fetchSingle("SELECT KURSLARARE FROM KURS WHERE KURSLARARE = "+lararid);
-            }
-            catch(InfException e){
-            }
-   
-            if(validering.kollaStringVarde(kompetens)){
-                try {
-                    idb.delete("DELETE FROM HAR_KOMPETENS_I WHERE LARAR_ID = "+lararid);
-                }
-                catch(InfException e){
+                idb.delete("DELETE FROM HAR_KOMPETENS_I WHERE LARAR_ID = " + lararid);
+            } catch (InfException e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
-                }
             }
-            if(validering.kollaStringVarde(kurslarare)){
-                try {
-                    idb.update("UPDATE KURS SET KURSLARARE = null WHERE KURSLARARE = "+lararid);
-                }
-                catch(InfException e){
+        }
+        if (validering.kollaStringVarde(kurslarare)) {
+            try {
+                idb.update("UPDATE KURS SET KURSLARARE = null WHERE KURSLARARE = " + lararid);
+            } catch (InfException e) {
                 JOptionPane.showMessageDialog(null, e.getMessage());
-                }
             }
- 
-            if(validering.kollaStringVarde(arforestandare)){
-                try {
-                    idb.delete("DELETE FROM LARARE WHERE LARAR_ID ="+ lararid);
-                    JOptionPane.showMessageDialog(null, "Läraren har tagits bort");
-                }
-                catch(InfException e){
+        }
+
+        if (!(validering.kollaStringVarde(arforestandare))) {
+            try {
+                idb.delete("DELETE FROM LARARE WHERE LARAR_ID =" + lararid);
+                JOptionPane.showMessageDialog(null, "Läraren har tagits bort");
+            } catch (InfException e) {
                 JOptionPane.showMessageDialog(null, "Kunde ej ta bort lärare med ID: " + lararid + e.getMessage());
-                }    
             }
-            else {
-                JOptionPane.showMessageDialog(null, "Läraren du försöker ta bort är föreståndare för " + arforestandare + ".  Ange ny föreståndare innan du tar bort den aktuella läraren.");
-            }  
+        } else {
+            JOptionPane.showMessageDialog(null, "Läraren du försöker ta bort är föreståndare för " + arforestandare + ".  Ange ny föreståndare innan du tar bort den aktuella läraren.");
+        }
     }//GEN-LAST:event_btnTaBortLarareActionPerformed
 
     private void btnLararInfoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLararInfoActionPerformed
